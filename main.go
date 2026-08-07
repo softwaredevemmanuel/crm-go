73c35fa691336563ec098e2397182a764ba774e6
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2026-08-07 02:22:48 +0100

    cors policy

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func main() {
 	r.Use(middleware.SessionMiddleware())
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://hr-app-emmadsaxlegends-projects.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "email"},
 		AllowCredentials: true,
